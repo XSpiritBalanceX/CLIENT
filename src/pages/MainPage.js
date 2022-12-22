b06e6a21b5789ac2c036a986977b396798653d1e
Storm <lenagreidon@mail.ru>
2022-12-22 15:38:31 +0300

    22.12

--- src/pages/MainPage.js
+++ src/pages/MainPage.js
@@ -1,7 +1,7 @@
-import React, {useEffect} from 'react';
+import React, {useEffect, useState} from 'react';
 import {connect} from 'react-redux';
 import {FormattedMessage} from 'react-intl';
-import {Container, Card} from 'react-bootstrap';
+import {Spinner, Card} from 'react-bootstrap';
 import mainImg from '../images/main.jpg';
 import {useParams } from 'react-router-dom';
 import decoded from 'jwt-decode';
@@ -10,6 +10,8 @@ import { loginUser } from '../redux/explainForReducer';
 
 const IntMainPage=(props)=>{
     const params=useParams();
+    const [isLoad, setLoad]=useState(false);
+    const [lastReview, setLast]=useState([])
     
     useEffect(()=>{ 
         if(params.token){
@@ -20,14 +22,28 @@ const IntMainPage=(props)=>{
         // eslint-disable-next-line
     },[])
 
+    useEffect(()=>{
+        fetch('http://localhost:5000/api/review/getmain')
+        .then(response=>response.json())
+        .then(data=>{console.log(data); setLast(data);setLoad(true)})
+        .catch(err=>console.log(err))
+        // eslint-disable-next-line
+    },[])
+
     return(
-        <Container className="d-flex justify-content-center align-items-center mainCont" style={{height:window.innerHeight-100}}>  
-          <Card style={{width:'70%', border:'solid 4px #9FA0A4'}} className='p-4 contMain'>
-          <Card.Img variant="top" src={mainImg} style={{width:'50%', height:'30%', borderRadius:'5px', margin:'0 auto 2% auto'}}/>
-          <Card.Title><FormattedMessage id='title'/></Card.Title>
-          <FormattedMessage id='gretings'/>
-          </Card>
-          </Container>
+        <div>
+            {isLoad?<React.Fragment><div className='helloMain'>
+                <Card style={{width:'70%', border:'solid 4px #9FA0A4'}} className='p-4 contMain'>
+                 <Card.Img variant="top" src={mainImg} style={{width:'30%', height:'5%', borderRadius:'5px', margin:'0 auto 2% auto'}}/> 
+                    <Card.Title><FormattedMessage id='title'/></Card.Title>
+                    <FormattedMessage id='gretings'/>
+                </Card>   
+            </div>
+            <div>
+                <div>Последние обзоры</div>
+            </div></React.Fragment>:
+                <Spinner animation="border" style={{position:'absolute', top:'50%', left:'50%'}}/>}
+        </div>
     )
 }
 
